1cf06bd36c1666acecc815d0c8bad68d79a3ecc6
Andrew Zhou <zyzhou@sfu.ca>
2017-09-22 01:26:34 -0700

    working webpage + fully parsed get and post requests done

---
+++ incident-report-system/src/main/java/app/LocationController.java
@@ -0,0 +1,14 @@
+package app;
+
+import Model.Location;
+import java.util.*;
+
+public class LocationController {
+
+    public List<Location> locationList = new ArrayList<>();
+
+    public List<Location> getAllLocations(){
+        return locationList;
+    }
+
+}
--- incident-report-system/src/main/java/app/TextBoxController.java
+++ incident-report-system/src/main/java/app/TextBoxController.java
@@ -2,12 +2,12 @@ package app;
 
 import spark.*;
 
-
+import Model.Location;
 
 public class TextBoxController {
 
     public static Route show = (Request request, Response response) -> {
-        response.redirect("/textbox.html");
+        response.redirect("/Form.html");
         return 1; //idk what else to return
     };
 }
