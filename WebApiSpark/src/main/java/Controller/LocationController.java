c6a2c1afd3098d62d4b5866c7d8bcb6b24325f45
Andrew Zhou <zyzhou@sfu.ca>
2017-09-26 00:27:53 -0700

    tracer bullets for spark framework

---
+++ WebApiSpark/src/main/java/Controller/LocationController.java
@@ -0,0 +1,53 @@
+package Controller;
+
+import DBConnector.Connector;
+import Model.Location;
+
+import java.sql.ResultSet;
+import java.util.*;
+import static Parser.JsonUtil.json;
+import static spark.Spark.*;
+
+public class LocationController {
+
+    public LocationController(){
+        setupEndPoints();
+    }
+
+    public List<Location> locationList = new ArrayList<>();
+
+    private void setupEndPoints() {
+        get("/locations", (request, response) -> {
+            return locationList;
+        }, json());
+
+        post("/locations", (request, response) -> {
+            String campus = request.queryParams("campus");
+            String building_num = request.queryParams("building_num");
+            int room_num = Integer.parseInt(request.queryParams("room_num"));
+            String department = request.queryParams("department");
+
+            Location loc = new Location(campus, building_num, room_num, department);
+            locationList.add(loc);
+            return "Success!";
+        }, json());
+
+        get("/test", (request, response) -> getClichedMessage());
+    }
+
+    public String getClichedMessage() {
+        try {
+            String query = "select * from employee";
+            ResultSet myRs = Connector.executeQuery(query);
+            while(myRs.next()){
+                String fname = myRs.getString("fname");
+                String lname = myRs.getString("lname");
+                System.out.println(fname + ' ' + lname);
+                return fname + ' ' + lname;
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return "Hello World";
+    }
+}
---
+++ WebApiSpark/src/main/java/Model/Location.java
@@ -0,0 +1,22 @@
+package Model;
+
+public class Location {
+
+    public Location()
+    {
+    }
+
+    public Location(String campus, String building_num, int room_num, String department){
+        this.campus = campus;
+        this.building_num = building_num;
+        this.room_num = room_num;
+        this.department = department;
+    }
+
+    private String campus;
+    private String building_num;
+    private int room_num;
+    private String department;
+
+
+}
