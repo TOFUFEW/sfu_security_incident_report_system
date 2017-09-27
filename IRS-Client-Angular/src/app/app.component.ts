1443e2e89c63fc676ad8e26384fc01c5aa7a5d30
rgabrina <rgabrina@sfu.ca>
2017-09-27 12:04:59 -0700

    Use a service to obtain data
    Move the locations array from app.component.ts into its own class.
    Instead of initializing locations in app.component.ts, call a method from LocationService

--- IRS-Client-Angular/src/app/app.component.ts
+++ IRS-Client-Angular/src/app/app.component.ts
@@ -1,37 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from './location';
+import { LocationService } from './location.service';
 
-/* Dummy data */
-const Locations: Location[] = [
-  {
-    campus: 'Vancouver',
-    building_num: 'HC',
-    room_num: 1,
-    department: 'SOSY'
-  },
-  {
-    campus: 'Vancouver',
-    building_num: 'HC',
-    room_num: 2,
-    department: 'SOSY'
-  },
-  {
-    campus: 'Vancouver',
-    building_num: 'HC',
-    room_num: 3,
-    department: 'SOSY'
-  }
-
-];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [LocationService]
 })
 
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SFU Incident Reporting System';
-  locations = Locations;
+  locations: Location[];
+
+  constructor(private locationService: LocationService) { }
+
+  getLocations(): void {
+    this.locations = this.locationService.getLocations();
+  }
+
+  ngOnInit() : void {
+    this.getLocations();
+  }
 }
---
+++ IRS-Client-Angular/src/app/mock-locations.ts
@@ -0,0 +1,24 @@
+/* Dummy data */
+import { Location } from './location';
+
+export const LOCATIONS: Location[] = [
+    {
+      campus: 'Burnaby',
+      building_num: 'ASB',
+      room_num: 5100,
+      department: 'CMPT'
+    },
+    {
+      campus: 'Vancouver',
+      building_num: 'Harbour Centre',
+      room_num: 2,
+      department: 'SOSY'
+    },
+    {
+      campus: 'Surrey',
+      building_num: 'SUR',
+      room_num: 3,
+      department: 'SOSY'
+    }
+  
+  ];
