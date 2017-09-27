0cead077dfce7c16b297186df8bbb55fec1d2353
rgabrina <rgabrina@sfu.ca>
2017-09-26 21:39:46 -0700

    WIP: Add a service to get locations data from web api

--- IRS-Client-Angular/src/app/app.component.ts
+++ IRS-Client-Angular/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Location } from './location';
+import { LocationService } from './locations.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [LocationService]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'SFU Incident Reporting System';
+
+  // --- Added to test location.service.ts
+  locations: Location[];
+
+  constructor(private locationService: LocationService) {}
+
+  getLocations(): void {
+    this.locationService.getLocations().then(locations => this.locations = locations);
+  }
+
+  ngOnInit(): void {
+    this.getLocations();
+  }
+  // -----
 }
---
+++ IRS-Client-Angular/src/app/location.ts
@@ -0,0 +1,6 @@
+export class Location {
+  campus: string;
+  building_num: string;
+  room_num: number;
+  department: string;
+}
