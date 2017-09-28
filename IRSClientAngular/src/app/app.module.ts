6f2bedf6388599dc87bcaf63980fd7606320fa33
rgabrina <rgabrina@sfu.ca>
2017-09-28 16:28:53 -0700

    Add http module

--- IRSClientAngular/src/app/app.module.ts
+++ IRSClientAngular/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
+import { Http, HttpModule, Headers } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -11,6 +12,7 @@ import { LocationComponent } from './location/location.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     HttpClientModule
   ],
   declarations: [
--- IRSClientAngular/src/app/location/location.service.ts
+++ IRSClientAngular/src/app/location/location.service.ts
@@ -1,30 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Location } from './location';
 //import { LOCATIONS } from '../mock-locations';
+import { Http, Headers } from '@angular/http';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class LocationService {
-    constructor(private http: HttpClient) {}
+    private headers = new Headers({'Content-Type': 'application/json'});
     locationsUrl = "http://localhost:4567/locations";
+    constructor(private http: Http) {}
 
     getLocations(): Promise<Location[]> {
         var locations = this.http.get(this.locationsUrl)
             .toPromise()
-            .then(response => response as Location[])
+            .then(response => response.json() as Location[])
             .catch(this.handleError);
         return Promise.resolve(locations);
     };
 
-    saveLocation(location: Location) : void {//Promise<any> {
-        // var promise = this.http
-        //         .post(this.locationsUrl, location)
-        //         .toPromise()
-        //         .then(response => response)
-        //         .catch(this.handleError);
-        // this.getLocations();
-        // return promise;
+    saveLocation(location: Location) : Promise<string> {
+        var promise = this.http
+                .post(this.locationsUrl, 
+                    JSON.stringify({campus: location.campus, building_num: location.building_num, room_num: location.room_num, department: location.department}), { headers: this.headers })
+                .toPromise()
+                .then(response => response.json() as string)
+                .catch(this.handleError);
+        this.getLocations();
+        return promise;
     };
 
     private handleError(error: any): Promise<any> {
