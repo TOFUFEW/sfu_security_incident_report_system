d457f7fa7faf493f77b0f10449610a6e456a08f8
Niccolo Pucci <npucci@sfu.ca>
2017-09-30 12:36:30 -0700

    Fix spacing of line 10 in JsonUtil.java

--- WebApiSpark/src/main/java/Parser/JsonUtil.java
+++ WebApiSpark/src/main/java/Parser/JsonUtil.java
@@ -7,7 +7,7 @@ public class JsonUtil
 {
     public static String toJson ( Object object )
     {
-        return new Gson ().toJson (object);
+        return new Gson ().toJson ( object );
     }
 
     public static ResponseTransformer json () {
