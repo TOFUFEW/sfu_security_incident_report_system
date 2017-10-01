a512d2e8a6e10f48959ead4a138d0c1f4c196721
mingnul <mingnul@sfu.ca>
2017-09-30 21:58:13 -0700

    basic routing to dashboard setup
    set up routing to dashboard to get ready to setup login page

--- IRSClientAngular/src/app/app.component.ts
+++ IRSClientAngular/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
+  selector: 'app-root',
+  template: `    
+    <nav>
+      <a routerLink="/dashboard">Dashboard</a>
+      <a routerLink="/login">Login</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `
 })
+
 export class AppComponent {
 
 }
--- IRSClientAngular/src/app/app.module.ts
+++ IRSClientAngular/src/app/app.module.ts
@@ -3,24 +3,34 @@ import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { Http, HttpModule, Headers } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule} from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LocationComponent } from './location/location.component';
+import { MyDashboardComponent} from "./myDashboard.component";
+import {LoginComponent} from "./login.component";
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,
     DashboardComponent,
-    LocationComponent
+    LocationComponent,
+    MyDashboardComponent
   ],
-  
+
   providers: [],
   bootstrap: [AppComponent]
 })
