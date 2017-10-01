efc6935d2f32cc24607976fd148fe3b131402ad3
rgabrina <rgabrina@sfu.ca>
2017-09-30 17:22:26 -0700

    If statement unnecessary

--- WebApiSpark/src/main/java/Util/JsonUtil.java
+++ WebApiSpark/src/main/java/Util/JsonUtil.java
@@ -11,12 +11,7 @@ public class JsonUtil {
         return gson.toJson(object);
     }
 
-    public static Object fromJson( String json , Class classOfT ) {
-        if ( classOfT == Location.class )
-            return gson.fromJson( json , classOfT );
-        // add more classes here
-        return null;
-    }
+    public static Object fromJson( String json , Class classOfT ) { return gson.fromJson( json , classOfT ); }
 
     public static ResponseTransformer json() {
         return JsonUtil::toJson;
