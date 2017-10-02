5520ec78fddcc56183cf704947f85cd9c9090bfc
mingnul <mingnul@sfu.ca>
2017-10-01 18:04:28 -0700

    show login screen on startup
    shows the login setup instead of dashboard first, have to add button functionality

--- IRSClientAngular/src/app/app.component.ts
+++ IRSClientAngular/src/app/app.component.ts
@@ -2,13 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  template: `    
-    <nav>
-      <a routerLink="/dashboard">Dashboard</a>
-      <a routerLink="/login">Login</a>
-    </nav>
-    <router-outlet></router-outlet>
-  `
+  templateUrl: 'login.component.html'
 })
 
 export class AppComponent {
