0716d43a60643218b1c33a4a65db0ee8c726042d
mingnul <mingnul@sfu.ca>
2017-10-01 19:24:59 -0700

    implement login button
    logging in with admin as username and password now routes the page to dashboard

--- IRSClientAngular/src/app/component/login.component.ts
+++ IRSClientAngular/src/app/component/login.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
-    selector: 'login-component',
     templateUrl: '../view/login.component.html'
 })
 
 export class LoginComponent {
-
+    data = <any>{};
+    constructor(private router: Router) {}
+    onLogin() {
+      if(this.data.username == "admin" && this.data.password == "admin") {
+          this.router.navigate(['dashboard']);
+      } else {
+          console.log("hi random");
+      }
+    }
 }
