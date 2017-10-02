f4eef1694bf4d81854e5e4a1fe2413acbbe13c90
Tyler <tarsenea@sfu.ca>
2017-10-02 11:45:06 -0700

    Add getLocation() to DBHelper.java

--- WebApiSpark/src/main/java/Util/DBHelper.java
+++ WebApiSpark/src/main/java/Util/DBHelper.java
@@ -1,6 +1,10 @@
 package Util;
 
+import DBConnector.Connector;
 import Model.*;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -50,5 +54,37 @@ public class DBHelper {
             }
         }
         return false;
+    }
+
+    public static List < Location > getLocations () {
+        List < Location > locationList = new ArrayList<>();
+
+        try
+        {
+            ResultSet result = Connector.executeQuery ( "select * from Location" );
+
+            while ( result.next() )
+            {
+                String locationId = result.getString ( "location_id" );
+                String campus = result.getString ( "campus" );
+                String building_num = result.getString ( "building_num" );
+                String room_num = result.getString ( "room_num" );
+                String department = result.getString ( "department" );
+
+                Location loc = new Location (
+                        locationId,
+                        campus,
+                        building_num,
+                        room_num,
+                        department
+                );
+                locationList.add(loc);
+            }
+        }
+        catch ( Exception e ) {
+            e.printStackTrace();
+        }
+
+        return locationList;
     }
 }
