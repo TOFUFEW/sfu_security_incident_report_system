de66dfc2d6f985ab4cd537a1d6c5857091c1b851
rgabrina <rgabrina@sfu.ca>
2017-10-10 15:35:36 -0700

    New incident modal

--- IRSClientAngular/src/app/model/location.ts
+++ IRSClientAngular/src/app/model/location.ts
@@ -6,25 +6,4 @@ export class Location {
   DEPARTMENT: string;
   CITY: string;
   ADDRESS: string;
-  
-  /*
-
-  Location = "{columValue:{ROOM_NUMBER:\"SURR-301\",CAMPUS_ID:\"1\",BUILDING_NUMBER:\"23\",DEPARTMENT:\"Sci\"}}"
-  Incident = "{ColumnValue:{}, Location, Staff}"
-
-
-[8:54] 
-  constructor ( 
-      id: number , 
-      campus: string ,
-      buildingNumber: string ,
-      roomNumber: number ,
-      department: string
-  ) {
-      this.id = id;
-      this.campus = campus;
-      this.buildingNumber = buildingNumber;
-      this.roomNumber = roomNumber;
-      this.department = department;
-  }*/
 }
