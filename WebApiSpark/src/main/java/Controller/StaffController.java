d17a4740362fa69786837d87543bd6d6bf90751a
Tyler <tarsenea@sfu.ca>
2017-10-10 17:01:13 -0700

    Update StaffController for modified selectIncidentElement

--- WebApiSpark/src/main/java/Controller/StaffController.java
+++ WebApiSpark/src/main/java/Controller/StaffController.java
@@ -1,6 +1,7 @@
 package Controller;
 import Model.Staff;
 import Util.DBHelper;
+import Util.DatabaseValues;
 import Util.JsonUtil;
 
 import java.util.*;
@@ -35,7 +36,7 @@ public class StaffController {
         put("/staff", (request, response) -> {
             Staff staff = ( Staff ) parser.fromJson( request.body () , Staff.class);
 
-            if (DBHelper.selectIncidentElement ( staff ) == null ) {
+            if (DBHelper.selectIncidentElement(staff, staff.getColumnValue( DatabaseValues.DatabaseColumn.PERSON_ID ) ) ) {
                 return -1;
             }
             return dbHelper.updateIncidentElement(staff);
