3c99e13a9100434e0f671f6e5f0fae2b6ac2eee6
nhann <nhann@sfu.ca>
2017-10-12 00:53:17 -0700

    Revise updateIncident procedure to modify relational tables
    Change updateIncident procedure to update the relational tables instead of just the incidents table

--- DatabaseScripts/CREATE PROCEDURE updateIncident.sql
+++ DatabaseScripts/CREATE PROCEDURE updateIncident.sql
@@ -1,13 +1,63 @@
 CREATE PROCEDURE [dbo].[updateIncident] 
 	@report_id INT,
 	@category_id INT,
-	@account_id INT,
 	@description TEXT,
 	@executive_summary TEXT,
-	@closed BIT
+	@closed BIT,
+	@location_id INT,
+	@person_id INT,
+	@staff_id INT
 AS
 BEGIN
 	UPDATE Incident
-	SET ACCOUNT_ID = @account_id, CATEGORY_ID = @category_id, DESCRIPTION = @description, EXECUTIVE_SUMMARY = @executive_summary, CLOSED = @closed
-	WHERE REPORT_ID = @report_id
+	SET CATEGORY_ID = @category_id, DESCRIPTION = @description, EXECUTIVE_SUMMARY = @executive_summary, CLOSED = @closed
+	WHERE REPORT_ID = @report_id;
+		
+	IF EXISTS ( SELECT LOCATION_ID
+		FROM Location
+		WHERE LOCATION_ID = @location_id
+	)
+	BEGIN
+		DELETE FROM HappensAt
+		WHERE REPORT_ID = @report_id;
+		INSERT INTO HappensAt (
+			REPORT_ID,
+			LOCATION_ID
+		) VALUES (
+			@report_id,
+			@location_id
+		);
+	END
+
+	IF EXISTS ( SELECT PERSON_ID
+		FROM Involves
+		WHERE PERSON_ID = @person_id
+	)
+	BEGIN
+		DELETE FROM Involves
+		WHERE REPORT_ID = @report_id;
+		INSERT INTO Involves (
+			REPORT_ID,
+			PERSON_ID
+		) VALUES (
+			@report_id,
+			@person_id
+		);
+	END
+
+	IF EXISTS ( SELECT ACCOUNT_ID
+		FROM Staff
+		WHERE ACCOUNT_ID = @staff_id
+	)
+	BEGIN
+		DELETE FROM AssignedTo
+		WHERE REPORT_ID = @report_id;
+		INSERT INTO AssignedTo (
+			REPORT_ID,
+			ACCOUNT_ID
+		) VALUES (
+			@report_id,
+			@staff_id
+		);
+	END
 END;
