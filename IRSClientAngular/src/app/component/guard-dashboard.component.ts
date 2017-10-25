110c65884a1b981f10f8d9c08bc1c0eee75bc2c1
mingnul <mingnul@sfu.ca>
2017-10-24 21:29:13 -0700

    set up clickable squares
    currently do nothing, trying to retrieve the correct incident at the moment

--- IRSClientAngular/src/app/component/guard-dashboard.component.ts
+++ IRSClientAngular/src/app/component/guard-dashboard.component.ts
@@ -3,7 +3,7 @@ import {Incident} from "../model/incident";
 import {IncidentService} from "../service/incident.service";
 import {User} from "../model/user";
 import {UserService} from "../service/user.service";
-
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'guard-dashboard',
@@ -18,7 +18,8 @@ export class GuardDashboardComponent implements OnInit {
   user: User;
 
   constructor( private incidentsService: IncidentService,
-               private userService: UserService ) {
+               private userService: UserService,
+               private router: Router) {
   };
 
   getIncidents(): void {
@@ -26,6 +27,20 @@ export class GuardDashboardComponent implements OnInit {
     this.incidentsService.getGuardIncidents( this.user ).then( returnedIncidents => {
       this.incidents = returnedIncidents;
     } );
+  }
+
+  viewReport( id: number ) : void {
+    console.log("view report");
+    console.log(id);
+    var i = 0;
+    for(i;i<this.incidents.length; i++) {
+      console.log(this.incidents[i]);
+      if(this.incidents[i].attributes.REPORT_ID == id) {
+        this.newIncident = this.incidents[i];
+      }
+    }
+    console.log(this.newIncident.attributes.REPORT_ID);
+    this.router.navigate(['guard']);
   }
 
   ngOnInit() : void {
