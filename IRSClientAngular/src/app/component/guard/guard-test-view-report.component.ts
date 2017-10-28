145e95041fca0eb23e10426a8796854abc80be29
mingnul <mingnul@sfu.ca>
2017-10-28 12:43:50 -0700

    missed files on previous commit

---
+++ IRSClientAngular/src/app/component/guard/guard-test-view-report.component.ts
@@ -0,0 +1,25 @@
+import 'rxjs/add/operator/switchMap';
+import {Component, OnInit} from "@angular/core";
+import {Incident} from "../report/incident";
+import {IncidentService} from "../../service/incident.service";
+import { ActivatedRoute , ParamMap } from "@angular/router";
+
+@Component({
+  selector: 'report',
+  templateUrl: './guard-test-view-report.html'
+})
+
+export class GuardTestViewReportComponent implements OnInit {
+  incident: Incident;
+
+  constructor( private incidentsService: IncidentService,
+               private router: ActivatedRoute ) {
+  };
+
+  ngOnInit(): void {
+      this.router.paramMap
+        .switchMap((params: ParamMap) => this.incidentsService.getIncident(+params.get('id')))
+        .subscribe(incident => this.incident = incident);
+      console.log(this.incident.attributes.REPORT_ID);
+  }
+}
