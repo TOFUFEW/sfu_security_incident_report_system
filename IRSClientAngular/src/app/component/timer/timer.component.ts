3a9fc625614dc5bc6536af5889cb47790d4f3016
Daniel Chen <sca169@sfu.ca>
2017-10-31 16:19:20 -0700

    add and display timers, working on countdown

--- IRSClientAngular/src/app/component/timer/timer.component.ts
+++ IRSClientAngular/src/app/component/timer/timer.component.ts
@@ -9,17 +9,22 @@ import { TimerService } from '../../service/timer.service';
 })
 
 export class TimerComponent implements OnInit {
-    timerList: Timer[];
+    timerList: Timer[] = new Array<Timer>();
     newTimer: Timer = new Timer;
 
     constructor(private timerService: TimerService){
         var timer1 : Timer = new Timer();
-        timer1.START_TIME = "2016-01-05T09:05:05";
-        timer1.END_TIME = "2016-01-05T09:05:05"
-     
-        timer2.START_TIME ='2016-01-05T09:05:05';
-        timer2.END_TIME ='2016-01-05T09:05:05';
+        timer1.TIMER_NAME = "Hello"
+        timer1.START_TIME = new Date("2016-01-05T09:05:05");
+        timer1.END_TIME = new Date("2017-10-31T17:05:05");
+        timer1.timeRemaining = 1;
+
         var timer2 : Timer = new Timer();
+        timer2.TIMER_NAME = "Goodbye"
+        timer2.START_TIME = new Date("2016-01-05T09:05:05");
+        timer2.END_TIME = new Date("2017-10-31T17:05:05");
+        timer2.timeRemaining = 0;
+        
         this.timerList.push(timer1);
         this.timerList.push(timer2);
     }
@@ -30,16 +35,37 @@ export class TimerComponent implements OnInit {
         // } );
     }
 
-    addTimer( timer : Timer ): void {
+    addTimer(): void {
         // this.timerService.update(timer).then( returnedTimer => {
         //     this.timerList.push(timer);
         // } );
-        this.timerList.push(timer);
+        this.timerList.push(this.newTimer);
+        var timerForm = document.getElementById("timerForm");
+        timerForm.style.display = "none";
     }
 
     deleteTimer(dtimer : Timer): void {
         var i = this.timerList.findIndex(timer => timer === dtimer)
         this.timerList.splice(i, 1);
+    }
+    
+    showAddTimer() : void {
+        var timerForm = document.getElementById("timerForm");
+        timerForm.style.display = "";
+    }
+    
+    start(): void {
+        //var x = setInterval(this.countDown(), 1000);
+    }
+    countDown() : void {
+        
+        // var now = new Date().getTime();
+        // this.timerList.forEach(timer => {
+        //     timer.timeRemaining = timer.END_TIME.getDate() - now;
+        //     var time = Math.floor((timer.timeRemaining % (1000 * 60 * 60)) / (1000 * 60))
+        //     document.getElementById("timerDisplay").innerHTML = time.toString();
+        // });
+
     }
 
 
--- IRSClientAngular/src/app/component/timer/timer.ts
+++ IRSClientAngular/src/app/component/timer/timer.ts
@@ -1,9 +1,10 @@
 export class Timer{
     TIMER_ID: number;
     TIMER_NAME: string;
-    START_TIME: string;
-    END_TIME: string;
-    DURATION: string;
-    PAUSE_DURATION: string;
+    START_TIME: Date;
+    END_TIME: Date;
+    DURATION: Date;
+    PAUSE_DURATION: Date;
+    timeRemaining: number;
 
 }
