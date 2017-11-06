57a5255a89d319de7bdff930ac09bcbe4a31f3a1
ivukmiro <ina_vukmirovich@sfu.ca>
2017-11-05 22:24:07 -0800

    Add status, start and end times to attributes in incident class

--- IRSClientAngular/src/app/component/report/incident.ts
+++ IRSClientAngular/src/app/component/report/incident.ts
@@ -39,6 +39,8 @@ export class IncidentAttributes {
     ACCOUNT_ID: number;
     DESCRIPTION: string;
     EXECUTIVE_SUMMARY: string;
-    CLOSED: number;
+    STATUS: number;
     CATEGORY_ID: number;
+    START_TIME: number;
+    END_TIME: number;
 }
