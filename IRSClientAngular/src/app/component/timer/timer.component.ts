2049bb1cdd14856a09843f2472076b48b95db6fa
Daniel Chen <sca169@sfu.ca>
2017-11-07 17:06:11 -0800

    storing timers as numbers and adjustments to go with it, fixed too many calls on load, custom string function

--- IRSClientAngular/src/app/component/timer/timer.component.ts
+++ IRSClientAngular/src/app/component/timer/timer.component.ts
@@ -10,23 +10,29 @@ import { TimerService } from '../../service/timer.service';
 })
 
 export class TimerComponent implements OnInit {
+    tempStart: string;
+    tempEnd: string;
     timerList: Timer[] = new Array<Timer>();
     newTimer: Timer = new Timer;
     timerFormShowing: boolean = false;
 
     constructor(private timerService: TimerService){
-        var now : number = Date.now();
+        var nowDate = new Date (Date.now());
+        console.log(nowDate.getHours());
+        console.log(nowDate.getMinutes());
+        var nowTime = nowDate.getHours() * 60 * 60 * 1000 + nowDate.getMinutes() * 60 * 1000;
+
         var timer1 : Timer = new Timer();
         timer1.TIMER_NAME = "Hello"
-        timer1.START_TIME = now - 10 * 60 * 1000;
-        timer1.END_TIME = now + 10 * 60 * 1000;
-        timer1.timeRemaining = now - timer1.START_TIME;
+        timer1.START_TIME = nowTime - 10 * 60 * 1000;
+        timer1.END_TIME = nowTime + 15 * 60 * 1000;
+        timer1.timeRemaining = timer1.END_TIME - nowTime;
 
         var timer2 : Timer = new Timer();
         timer2.TIMER_NAME = "Goodbye"
-        timer2.START_TIME = now - 15 * 60 * 1000;
-        timer2.END_TIME = now + 15 * 60 * 1000;
-        timer2.timeRemaining = now - timer2.START_TIME;
+        timer2.START_TIME = nowTime - 15 * 60 * 1000;
+        timer2.END_TIME = nowTime + 15 * 60 * 1000;
+        timer2.timeRemaining = timer2.END_TIME - nowTime;
         
         this.timerList.push(timer1);
         this.timerList.push(timer2);
@@ -42,6 +48,10 @@ export class TimerComponent implements OnInit {
         // this.timerService.update(timer).then( returnedTimer => {
         //     this.timerList.push(timer);
         // } );
+        var splice: string[] = this.tempStart.split(":");
+        this.newTimer.START_TIME = parseInt(splice[0])* 60 * 60 * 1000;
+        this.newTimer.END_TIME = parseInt(splice[1]) * 60 * 1000;
+
         this.timerList.push(this.newTimer);
         var timerForm = document.getElementById("timerForm");
         timerForm.style.display = "none";
@@ -63,30 +73,44 @@ export class TimerComponent implements OnInit {
 
        }
     }
-
-    setStartTimePlaceholder() : void {
-            
-    }
     
     start(): void {
-        //var x = setInterval(() => this.countDown(), 1000);
+        var x = setInterval(() => this.countDown(), 1000);
+        console.log("hi");
     }
 
     countDown() : void {
-        var now = new Date().getTime();
+        
         this.timerList.forEach(timer => {
-            timer.timeRemaining-- ;
+            timer.timeRemaining= timer.timeRemaining - 1000 ;
             console.log(timer.timeRemaining)
+            if (timer.timeRemaining <= 0){
+                alert(timer.TIMER_NAME + " Times up!");
+                this.endTimer(timer);
+            }
         });
 
     }
+    endTimer(timer: Timer) : void {
 
-    log(message: string) : void{
-        console.log(message);
+    }
+
+    timeToString(time : number ) : string {
+        console.log(time);
+        var hour = Math.floor(time/1000/60/60%24).toString();
+        var minute = Math.floor(time/1000/60%60).toString();
+        var second = (time/1000%60).toString();
+
+        return hour + ":" + minute + ":" + second;
     }
 
     ngOnInit(){
         this.getTimers();
+    }
+
+
+    ngAfterViewInit(){
+        this.start();
     }
 
 }
