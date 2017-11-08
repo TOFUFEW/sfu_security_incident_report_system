446c47d856b39c53ad476401a7acbba90413a6fc
nhann <nhann@sfu.ca>
2017-11-08 00:46:26 -0800

    Create script to get incident depending on account id

---
+++ DatabaseScripts/CREATE PROCEDURE getIncidents.sql
@@ -0,0 +1,22 @@
+CREATE PROCEDURE [dbo].[getIncidents]
+	@user_id INT
+AS
+BEGIN
+	IF (SELECT ACCOUNT_ID
+		FROM Account
+		WHERE ACCOUNT_TYPE = @user_id) = 1
+	BEGIN
+		SELECT * 
+		FROM Incident 
+		ORDER BY REPORT_ID
+		DESC;
+	END
+	ELSE
+	BEGIN
+		SELECT Incident.*
+		FROM Incident INNER JOIN AssignedTo ON Incident.REPORT_ID =	AssignedTo.REPORT_ID
+		WHERE AssignedTo.ACCOUNT_ID = @user_id and Incident.STATUS != 4
+		ORDER BY REPORT_ID
+		DESC;
+	END
+END;
