bfc8a896b04f9123773edc03ab6198cf5bca4ca6
rgabrina <rgabrina@sfu.ca>
2017-11-19 14:56:07 -0800

    Edit desc and summary

--- IRSClientAngular/src/app/component/report/report-summary.component.ts
+++ IRSClientAngular/src/app/component/report/report-summary.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Config } from '../../util/config.service';
 import { Incident } from '../report/incident';
 import { IncidentService } from '../../service/incident.service';
+import { LocationService } from '../../service/location.service';
+import { map } from 'rxjs/operators/map';
 
 @Component({
     selector: 'report-summary',
@@ -12,24 +14,61 @@ import { IncidentService } from '../../service/incident.service';
 export class ReportSummaryComponent implements OnInit {
     @Input() inputReport: Incident;
     report: Incident;
-
+    report_edit: Incident;
+    editMode: boolean = false;
     constructor (
-        private incidentService: IncidentService
+        private incidentService: IncidentService,
+        private locationService: LocationService
     ) { 
-        //this.report = new Incident();
-        console.log(this.report);
     }
 
     removeFromWorkspace( id: number ): void {
         this.incidentService.removeFromWorkspace( id );
     }
 
+    toggleEditMode(){
+        this.editMode = !this.editMode;
+    }
+
+    cancelUpdate() {
+        this.report_edit = this.copyReportUnbounded( this.report );
+        this.editMode = false;
+    }
+
+    updateReport() {
+        this.report = this.report_edit;
+        this.editMode = false;
+        this.incidentService.update( this.report );
+    }
+
+    addIncidentElement( type: string ) {
+        if ( type == 'location' ) {
+
+        }
+    }
+
     ngOnInit(): void {
         if ( this.inputReport != null) {
             this.report = this.inputReport;
-        }
-            
-        console.log( this.report )
-        console.log ( "report category ", this.report.category);                
+            this.cancelUpdate();
+        }          
+    }
+
+    private copyReportUnbounded( source: Incident ): Incident {
+        var incident = new Incident();
+        incident.attributes.ACCOUNT_ID = source.attributes.ACCOUNT_ID;
+        incident.attributes.CATEGORY_ID = source.attributes.CATEGORY_ID;
+        incident.attributes.DESCRIPTION = source.attributes.DESCRIPTION;
+        incident.attributes.EXECUTIVE_SUMMARY = source.attributes.EXECUTIVE_SUMMARY;
+        incident.attributes.REPORT_ID = source.attributes.REPORT_ID;
+        incident.attributes.STATUS = source.attributes.STATUS;
+        incident.incidentElements = source.incidentElements;
+        incident.category = source.category;
+        incident.guard = source.guard;
+        incident.inWorkspace = source.inWorkspace;
+        incident.searchString = source.searchString;
+        incident.table = source.table;
+
+        return incident;
     }
 }
