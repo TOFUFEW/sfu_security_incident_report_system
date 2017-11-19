6bf3e89b26cc86cf9f10779a64e6c937c1183672
rgabrina <rgabrina@sfu.ca>
2017-11-18 20:29:17 -0800

    navbar links

--- IRSClientAngular/src/app/app.component.ts
+++ IRSClientAngular/src/app/app.component.ts
@@ -9,28 +9,32 @@ import { Router, RouterModule } from '@angular/router';
 
 export class AppComponent implements OnInit {
 
-  constructor( private router: Router,
-               private userService: UserService ) {
-  }
+    constructor( private router: Router,
+                private userService: UserService ) {
+    }
 
-  showLogoutButton() {
-      var button = document.getElementById("logout" );
-      button.style.visibility = "visible";
-  }
+    showLogoutButton() {
+        var button = document.getElementById("logout" );
+        button.style.visibility = "visible";
+    }
 
-  onLogout() {
-      var button = document.getElementById("logout" );
-      button.style.visibility = "hidden";
-      this.userService.logout();
-      this.router.navigate([ 'login' ] );
-  }
+    onLogout() {
+        var button = document.getElementById("logout" );
+        button.style.visibility = "hidden";
+        this.userService.logout();
+        this.router.navigate([ 'login' ] );
+    }
 
-  ngOnInit(): void {
-      var button = document.getElementById("logout" );
-      if( this.userService.isLoggedIn() ) {
-          button.style.visibility = "visible";
-      } else {
-          button.style.visibility = "hidden";
-      }
-  }
+    redirect( component: string ) {
+        this.router.navigate([component]);
+    }
+
+    ngOnInit(): void {
+        var button = document.getElementById("logout" );
+        if( this.userService.isLoggedIn() ) {
+            button.style.visibility = "visible";
+        } else {
+            button.style.visibility = "hidden";
+        }
+    }
 }
