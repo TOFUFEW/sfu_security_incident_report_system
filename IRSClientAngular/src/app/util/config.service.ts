ff8164e0058eb89779917c02cfd06ce9ac8a501d
nhann <nhann@sfu.ca>
2017-11-20 15:46:36 -0800

    Change http to https

--- IRSClientAngular/src/app/util/config.service.ts
+++ IRSClientAngular/src/app/util/config.service.ts
@@ -1,15 +1,15 @@
 export class Config {
 
-    static LocationsURI: string = "http://localhost:4567/locations";
-    static IncidentsURI: string = "http://localhost:4567/incidents";
-    static StaffURI: string = "http://localhost:4567/staff";
-    static LoginURI: string = "http://localhost:4567/login";
-    static PersonURI: string = "http://localhost:4567/person";
-    static GetIncidentsURI: string = "http://localhost:4567/get-incidents";
-    static GetIncidentURI: string = "http://localhost:4567/get-incident";
-    static CategoriesURI: string = "http://localhost:4567/categories";
-    static UpdateIncidentURI: string = "http://localhost:4567/update-incident";
-    static UploadURI: string = "http://localhost:4567/upload";
+    static LocationsURI: string = "https://localhost:4567/locations";
+    static IncidentsURI: string = "https://localhost:4567/incidents";
+    static StaffURI: string = "https://localhost:4567/staff";
+    static LoginURI: string = "https://localhost:4567/login";
+    static PersonURI: string = "https://localhost:4567/person";
+    static GetIncidentsURI: string = "https://localhost:4567/get-incidents";
+    static GetIncidentURI: string = "https://localhost:4567/get-incident";
+    static CategoriesURI: string = "https://localhost:4567/categories";
+    static UpdateIncidentURI: string = "https://localhost:4567/update-incident";
+    static UploadURI: string = "https://localhost:4567/upload";
 
     static StaffTable: string = "STAFF";
     static LocationTable: string = "LOCATION";
