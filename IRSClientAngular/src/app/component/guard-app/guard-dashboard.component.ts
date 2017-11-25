6e0fa58b19783e57ff868d1cb11b739247152c99
mingnul <mingnul@sfu.ca>
2017-11-24 23:33:06 -0800

    refactor

--- IRSClientAngular/src/app/component/guard-app/guard-dashboard.component.ts
+++ IRSClientAngular/src/app/component/guard-app/guard-dashboard.component.ts
@@ -1,44 +1,56 @@
-import {Component, OnInit} from "@angular/core";
-import {IncidentService} from "../../service/incident.service";
-import {UserService} from "../../service/user.service";
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
 import { Router, RouterModule } from '@angular/router';
-import {Incident} from "../report/incident";
-import {User} from "../login/user";
+
+import { IncidentService } from "../../service/incident.service";
+import { UserService } from "../../service/user.service";
+import { Incident } from "../report/incident";
+import { User } from "../login/user";
 
 @Component({
   selector: 'guard-dashboard',
-  templateUrl: './guard-dashboard.component.html',
-  styleUrls: ['../../../assets/css/guard-dashboard.component.css'],
+  templateUrl: './guard-dashboard.html',
+  styleUrls: ['../../../assets/css/guard-app.css'],
   providers: [ IncidentService ]
 })
 
 export class GuardDashboardComponent implements OnInit {
-  incidents: Incident[];
-  user: User;
+    assignedIncidents: Incident[];
+    createdIncidents: Incident[];
+    user: User;
 
-  constructor( private incidentsService: IncidentService,
-               private userService: UserService,
-               private router: Router) {
-    if ( this.userService.isLoggedIn() == false ) {
-      this.router.navigate([ 'login' ] );
+    constructor( private incidentsService: IncidentService,
+                 private userService: UserService,
+                 private http: HttpClient,
+                 private router: Router) {
+    };
+
+    getAssignedIncidents(): void {
+        this.user = this.userService.getCurrentUser();
+        this.incidentsService.getGuardIncidents( ).then( returnedIncidents => {
+            console.log("returned incidents: ", returnedIncidents);
+            this.assignedIncidents = returnedIncidents;
+        } );
     }
-  };
 
-  getIncidents(): void {
-    this.user = this.userService.getCurrentUser();
-    this.incidentsService.getIncidents( ).then( returnedIncidents => {
-      this.incidents = returnedIncidents;
-    } );
-  }
+    getCreatedIncidents(): void {
+        this.incidentsService.getCreatedByIncidents().then( returnedIncidents => {
+            this.createdIncidents = returnedIncidents;
+        } );
+    }
 
-  viewReport( incident: Incident ) : void {
-    //TO BE CHANGED
-    //change to actually view report when feature is added
-    console.log("view report id = " + incident.attributes.REPORT_ID );
-    this.router.navigate([ 'report', incident.attributes.REPORT_ID ] );
-  }
+    viewReport( incident: Incident ) : void {
+        console.log("view report id = " + incident.attributes.REPORT_ID );
+        this.router.navigate([ 'guard-app/report', incident.attributes.REPORT_ID ] );
+    }
 
-  ngOnInit() : void {
-    this.getIncidents();
-  }
+    newReport(): void {
+        console.log("new report");
+        this.router.navigate([ 'new-report' ] );
+    }
+
+    ngOnInit() : void {
+        this.getAssignedIncidents();
+        this.getCreatedIncidents();
+    }
 }
