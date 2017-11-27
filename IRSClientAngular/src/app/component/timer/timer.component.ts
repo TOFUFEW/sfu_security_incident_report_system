0f9bb3fe7b52562e82686ccd31a56562c2329667
Daniel Chen <sca169@sfu.ca>
2017-11-26 17:34:47 -0800

    removed unused code and cleaned up code to match style

--- IRSClientAngular/src/app/component/timer/timer.component.ts
+++ IRSClientAngular/src/app/component/timer/timer.component.ts
@@ -4,8 +4,6 @@ import { Timer } from './timer';
 import { TimerService } from '../../service/timer.service';
 import { IncidentService } from '../../service/incident.service';
 
-
-
 @Component({
     selector: 'timer-component',
     templateUrl: './timer.component.html',
@@ -18,166 +16,116 @@ export class TimerComponent implements OnInit {
     timerList: Timer[] = new Array<Timer>();
     newTimer: Timer = new Timer();
     modifyValid = true;
-    
 
-
-    constructor(private timerService: TimerService, private incidentService: IncidentService){
-        var nowDate = new Date (Date.now());
+    constructor( private timerService: TimerService, private incidentService: IncidentService ) {
+        var nowDate = new Date ( Date.now() );
         this.timeNow = nowDate.getHours() * 60 * 60 * 1000 + nowDate.getMinutes() * 60 * 1000;
 
         this.incidentService.editedReport
         .subscribe( value => {
-            if ( value != null && this.timerList!= null && this.timerList.length > 0 && value.attributes.TIMER_START != null && value.attributes.TIMER_END!= null) {
+            if ( value != null && this.timerList!= null && this.timerList.length > 0 && value.attributes.TIMER_START != null && value.attributes.TIMER_END!= null ) {
                 var index = this.timerList.findIndex( i => i.incident.attributes.REPORT_ID == value.attributes.REPORT_ID );
                 if ( index >= 0 && value.attributes ) {
-                    this.timerList.splice(index, 1, this.timerService.createTimerInt(value, value.attributes.TIMER_START, value.attributes.TIMER_END));
+                    this.timerList.splice( index, 1, this.timerService.createTimerInt( value, value.attributes.TIMER_START, value.attributes.TIMER_END ) );
                 } else {
-                    this.timerList.push(this.timerService.createTimerInt(value, value.attributes.TIMER_START, value.attributes.TIMER_END));
+                    this.timerList.push( this.timerService.createTimerInt( value, value.attributes.TIMER_START, value.attributes.TIMER_END ) );
                 }
                 this.sortTimers();
             }
-        });
-
-        //TEST TIMERS
-
-        // var timer1 : Timer = new Timer();
-        // timer1.TIMER_START = this.timeNow - 10 * 60 * 1000;
-        // timer1.TIMER_END = this.timeNow + 15 * 60 * 1000;
-        // timer1.TIME_REMAINING = timer1.TIMER_END - this.timeNow;
-
-        // var timer2 : Timer = new Timer();
-        // timer2.TIMER_START = this.timeNow - 15 * 60 * 1000;
-        // timer2.TIMER_END = this.timeNow + 20 * 60 * 1000;
-        // timer2.TIME_REMAINING = timer2.TIMER_END - this.timeNow;
-        
-        // this.timerList.push(timer1);
-        // this.timerList.push(timer2);
-
-        //END TEST TIMERS
-        
+        });        
     }
 
-    getTimers(): void {
+    getTimers (): void {
        this.timerService.getTimers().then(returnedTimers => {
            this.timerList = returnedTimers;
-       }).then(()=> {this.sortTimers()});
+       }).then( ()=> { this.sortTimers() });
     }
 
-    // checkInput() : void{
-    //     if(this.tempStart > this.tempEnd){
-    //         alert("End time cannot be before start time");
-    //     } else {
-    //         this.addTimer();
-    //     }
-    // }
-
-    // addTimer(): void {
-    //     this.newTimer.TIMER_START = this.timerService.stringToTime(this.tempStart);
-    //     this.newTimer.TIMER_END = this.timerService.stringToTime(this.tempEnd);
-
-    //     this.tempStart = "";
-    //     this.tempEnd = "";
-
-    //     this.newTimer.TIME_REMAINING = this.newTimer.TIMER_END - this.timeNow;
-
-    //     this.timerList.push(this.newTimer);        
-    // }
-
-    sortTimers() : void {
-        this.timerList.sort( (a , b) => {
-            if (a.TIME_REMAINING < b.TIME_REMAINING){
+    sortTimers () : void {
+        this.timerList.sort( ( a , b ) => {
+            if ( a.TIME_REMAINING < b.TIME_REMAINING ){
                 return -1
-            } else if (a.TIME_REMAINING > b.TIME_REMAINING) {
+            } else if ( a.TIME_REMAINING > b.TIME_REMAINING ) {
                 return 1;
             } else {
                 return 0;
             }
-        })
+        });
     }
     
-    start(): void {
-        var x = setInterval(() => this.countDown(), 1000);
-
+    start (): void {
+        var x = setInterval( () => this.countDown(), 1000 );
     }
 
-    countDown() : void {
-        this.timerList.forEach(timer => {
+    countDown  () : void {
+        this.timerList.forEach( timer => {
             this.timeNow = this.timeNow - 1000;
             timer.TIME_REMAINING= timer.TIME_REMAINING - 1000 ;
-            if (timer.TIME_REMAINING <= 0){
-                this.timeUp(timer);
+            if ( timer.TIME_REMAINING <= 0 ) {
+                this.timeUp( timer );
             }
         });
-
     }
 
-    onChangeModifyTimer() : void {
-        if (this.tempStart == null || this.tempEnd == null){
+    onChangeModifyTimer () : void {
+        if ( this.tempStart == null || this.tempEnd == null ) {
             this.modifyValid = false;
         } else {
             this.modifyValid = true;
         }
     }
 
-    modifyTimer( timer : Timer ) : void {
-        timer.TIMER_START = this.timerService.stringToTime(this.tempStart);
-        timer.TIMER_END = this.timerService.stringToTime(this.tempEnd);
+    modifyTimer ( timer : Timer ) : void {
+        timer.TIMER_START = this.timerService.stringToTime( this.tempStart );
+        timer.TIMER_END = this.timerService.stringToTime( this.tempEnd );
         timer.TIME_REMAINING = timer.TIMER_END - this.timeNow;
 
         timer.incident.attributes.TIMER_START = timer.TIMER_START;
         timer.incident.attributes.TIMER_END = timer.TIMER_END;
-        this.incidentService.update(timer.incident);
+        this.incidentService.update( timer.incident );
         this.tempStart = "";
         this.tempEnd = "";
     }
 
-    timeUp(timer : Timer) : void{
-        if(confirm ("Time up for" + timer.incident.attributes.EXECUTIVE_SUMMARY + ", repeat timer?")){
+    timeUp ( timer : Timer ) : void {
+        if(confirm ( "Time up for" + timer.incident.attributes.EXECUTIVE_SUMMARY + ", repeat timer?" )) {
             var temp = timer.TIMER_END;
-            timer.TIMER_END = timer.TIMER_END + (timer.TIMER_END - timer.TIMER_START);
+            timer.TIMER_END = timer.TIMER_END + ( timer.TIMER_END - timer.TIMER_START );
             timer.TIMER_START = temp;
             timer.TIME_REMAINING = timer.TIMER_END - timer.TIMER_START;
             timer.TIMER_END - timer.TIMER_START;
         } else {
-            this.deleteTimer(timer);
+            this.deleteTimer( timer );
         } 
-
     }
 
-    removeTimer(timer : Timer) : void {
-        if (confirm("delete timer?")){
-            this.deleteTimer(timer);
+    removeTimer ( timer : Timer ) : void {
+        if ( confirm("delete timer?") ) {
+            this.deleteTimer( timer );
         }
     }
 
-    deleteTimer(dtimer : Timer): void {
-        this.timerService.deleteTimer(dtimer);
-        var i = this.timerList.findIndex(timer => timer === dtimer)
-        this.timerList.splice(i, 1);
+    deleteTimer ( dtimer : Timer ) : void {
+        this.timerService.deleteTimer( dtimer );
+        var i = this.timerList.findIndex( timer => timer === dtimer )
+        this.timerList.splice( i, 1 );
     }
     
 
-    timeToString(time : number) : string {
-        return this.timerService.timeToString(time);
+    timeToString ( time : number ) : string {
+        return this.timerService.timeToString( time );
     }
 
-    durationToString(time : number) : string {
-        return this.timerService.durationToString(time);
+    durationToString ( time : number ) : string {
+        return this.timerService.durationToString( time );
     }
 
 
-
-    pauseTimer(timer : Timer) : void {
-        
-    }
-
-    ngOnInit(){
+    ngOnInit () {
         this.getTimers();
     }
 
-
-    ngAfterViewInit(){
+    ngAfterViewInit () {
         this.start();
     }
-
 }
--- IRSClientAngular/src/app/component/timer/timer.ts
+++ IRSClientAngular/src/app/component/timer/timer.ts
@@ -1,14 +1,9 @@
 import { Incident } from '../report/incident';
 
-export class Timer{
+export class Timer {
     incident: Incident;
     TIMER_START: number;
     TIMER_END: number;
     TIME_REMAINING: number;
-    modify: boolean;
-    // interval: NodeJS.Timer;
-
-    constructor(){
-        this.modify = false;
-    }
+    modify: boolean = false;
 }
