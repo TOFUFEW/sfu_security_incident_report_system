b9833f4411b9e214023581ceac5fe3b4eb98a14d
rgabrina <rgabrina@sfu.ca>
2017-11-29 15:53:45 -0800

    Built jar for vm deployment

--- IRSClientAngular/src/app/util/config.service.ts
+++ IRSClientAngular/src/app/util/config.service.ts
@@ -1,35 +1,35 @@
 export class Config {
 
-    private static serverURL: string = "https://localhost:4567/";
+     private static protocol: string = "https://";
+    // private static serverURL: string = "localhost:4567/";
+    private static serverURL: string = "cmpt373-1177a.cmpt.sfu.ca:4567/";
     
-    //private static serverURL: string = "https://cmpt373-1177a.cmpt.sfu.ca:4567/";
-    
-    static LocationsURI: string = Config.serverURL + "locations";
-    static IncidentsURI: string = Config.serverURL + "incidents";
-    static FTSearchURI: string = Config.serverURL + "ftsearch-incident";
-    static CTSearchURI: string = Config.serverURL + "ctsearch-incident";
-    static StaffURI: string = Config.serverURL + "staff";
-    static LoginURI: string = Config.serverURL + "login";
-    static PersonURI: string = Config.serverURL + "person";
+    static LocationsURI: string = Config.protocol + Config.serverURL + "locations";
+    static IncidentsURI: string = Config.protocol + Config.serverURL + "incidents";
+    static FTSearchURI: string = Config.protocol + Config.serverURL + "ftsearch-incident";
+    static CTSearchURI: string = Config.protocol + Config.serverURL + "ctsearch-incident";
+    static StaffURI: string = Config.protocol + Config.serverURL + "staff";
+    static LoginURI: string = Config.protocol + Config.serverURL + "login";
+    static PersonURI: string = Config.protocol + Config.serverURL + "person";
 
-    static GetIncidentsURI: string = Config.serverURL + "get-incidents";
-    static GetIncidentURI: string = Config.serverURL + "get-incident";
-    static CreatedIncidentsURI: string = Config.serverURL + "created-incidents";
-    static CategoriesURI: string = Config.serverURL + "categories";
-    static UpdateIncidentURI: string = Config.serverURL + "update-incident";
-    static UploadURI: string = Config.serverURL + "upload";
-    static TimerURI: string = Config.serverURL + "timer";
-    static UploadsURI: string = Config.serverURL + "uploads";
-    static GetFileURI: string = Config.serverURL + "upload/get/";
-    static NewAccountURI: string = Config.serverURL + "create-account";
-    static GetAccountTypesURI: string = Config.serverURL + "get-account-types";
-    static GetCampusURI: string = Config.serverURL + "campus";
-    static ValidateUsernameURI: string = Config.serverURL + "validate-username";
-    static PersonExistsURI: string = Config.serverURL + "person-exists";
-    static WSURI: string = "ws://localhost:4567/ws";
+    static GetIncidentsURI: string = Config.protocol + Config.serverURL + "get-incidents";
+    static GetIncidentURI: string = Config.protocol + Config.serverURL + "get-incident";
+    static CreatedIncidentsURI: string = Config.protocol + Config.serverURL + "created-incidents";
+    static CategoriesURI: string = Config.protocol + Config.serverURL + "categories";
+    static UpdateIncidentURI: string = Config.protocol + Config.serverURL + "update-incident";
+    static UploadURI: string = Config.protocol + Config.serverURL + "upload";
+    static TimerURI: string = Config.protocol + Config.serverURL + "timer";
+    static UploadsURI: string = Config.protocol + Config.serverURL + "uploads";
+    static GetFileURI: string = Config.protocol + Config.serverURL + "upload/get/";
+    static NewAccountURI: string = Config.protocol + Config.serverURL + "create-account";
+    static GetAccountTypesURI: string = Config.protocol + Config.serverURL + "get-account-types";
+    static GetCampusURI: string = Config.protocol + Config.serverURL + "campus";
+    static ValidateUsernameURI: string = Config.protocol + Config.serverURL + "validate-username";
+    static PersonExistsURI: string = Config.protocol + Config.serverURL + "person-exists";
+    static WSURI: string = "ws://" + Config.serverURL + "ws";
 
-    static IncidentsWebSocketURI: string = "wss://localhost:4567/incidentsWebSocket";
-    static LogInWebSocketURI: string = "wss://localhost:4567/loginWebSocket";
+    static IncidentsWebSocketURI: string = "wss://" + Config.serverURL + "incidentsWebSocket";
+    static LogInWebSocketURI: string = "wss://" + Config.serverURL + "loginWebSocket";
 
     static StaffTable: string = "STAFF";
     static LocationTable: string = "LOCATION";
